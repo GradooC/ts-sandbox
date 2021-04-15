6e4158f0f61a204859f5abbaa620eaaa3c2f4b1a
Щемелёв Евгений Владимирович <ESchemelev@alfabank.ru>
2021-04-15 22:41:06 +0300

    improve the doubleArray function

--- doubleArray.ts
+++ doubleArray.ts
@@ -1,9 +1,7 @@
-// const f = (arr: number[]): number[] => arr.filter(Boolean).map((el) => el * 2);
-const f = (arr: number[]): number[] =>
+export const doubleArray = (arr: number[]): number[] =>
     arr.reduce((acc, el) => (el ? [...acc, el * 2] : [...acc]), []);
 
-f([1, 2, null, 7, 8, null, 3]); // => [2, 4, 14, 16, 6]
-console.log(
-    '🚀 ~ file: doubleArray.ts ~ line 4 ~ f([1, 2, null, 7, 8, null, 3])',
-    f([1, 2, null, 7, 8, null, 3])
-);
+console.time('Executing');
+const res = doubleArray([1, 2, null, 7, 8, null, 3]);
+console.timeEnd('Executing');
+console.log(res);
