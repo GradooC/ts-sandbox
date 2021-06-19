b62322c2f4514a9791db6c79fd6f95aee2a4c79b
Щемелёв Евгений Владимирович <ESchemelev@alfabank.ru>
2021-06-19 15:54:14 +0300

    restructure files

--- src/debounce.ts
+++ src/debounce.ts
@@ -5,32 +5,40 @@ const assertEquals = (a: any, b: any) => {
         console.log(`${a} === ${b}`);
         return true;
     }
-    console.log(`${a} !== ${b}`);
-    return false;
+    throw new Error(`${a} !== ${b}`);
 };
 
 (async () => {
     const debounce = <
         F extends (...args: any[]) => any,
-        Args extends Parameters<F>
+        A extends Parameters<F>
     >(
         fn: F,
-        ms: number = 0
+        delay: number
     ) => {
-        let timerId: ReturnType<typeof setTimeout>;
-        return (...args: Args) => {
-            clearTimeout(timerId);
-            timerId = setTimeout(fn, ms, args);
+        let isBlocked = false;
+
+        return (...args: A) => {
+            if (isBlocked) return;
+
+            fn(...args);
+            isBlocked = true;
+
+            setTimeout(() => {
+                isBlocked = false;
+            }, delay);
         };
     };
 
     let counter = 0;
     const updateState = (num: number = 1, str: string = '') => {
-        counter + num;
+        counter += num;
     };
+
     const debouncedUpdate = debounce(updateState, 300);
-    debouncedUpdate(2); // counter == 1
-    debouncedUpdate(); // counter == 1
+
+    debouncedUpdate(1); // counter == 1
+    debouncedUpdate(1); // counter == 1
     await delay(300); // counter == 1
     assertEquals(counter, 1);
     debouncedUpdate(); // counter == 2
